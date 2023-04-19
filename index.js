138c4dc9987f5b0dfdd233f6ef7566679109b8e0
Dhruv <dhruvpurwar15@gmail.com>
2023-04-19 16:37:58 +0530

    Added steps

--- index.js
+++ index.js
@@ -10,3 +10,15 @@
 const DataStructures = require("./dataStructures/index.js");
 
 module.exports = { DataStructures };
+
+// Steps
+// 1. Add unique package name when doing npm init. Also add other info
+// 2. Create intertwined module.exports as shown here.
+// 3. Do git init, add and commit files.
+// 4. Push on your github as private /public .
+// 5. Type npm login. It will ask credentials.
+// 6. Do npm publish. Your package will get created.
+// 7. Test your package by installing it and trying its commands.
+// 8. Now , create a README file , so your npm package page show that file.
+
+//  Refer https://docs.npmjs.com/creating-and-publishing-private-packages/
